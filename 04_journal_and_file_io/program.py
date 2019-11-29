c43a25dd51ee3fc86113beb33aad96bd65d8ad00
vivekvashist <vivekvashist@gmail.com>
2019-11-29 23:49:29 +0000

    added journal app

---
+++ 04_journal_and_file_io/program.py
@@ -0,0 +1,18 @@
+def print_header():
+    print("--------------------------------")
+    print("           JOURNAL APP")
+    print("--------------------------------")
+    print()
+
+
+def run_event_loop():
+    pass
+
+
+def main():
+    print_header()
+    run_event_loop()
+
+
+if __name__ == "__main__":
+    main()
