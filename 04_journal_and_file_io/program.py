fce01789932b346b9d55402360f2ce9c16714d9e
vivekvashist <vivekvashist@gmail.com>
2019-11-30 01:03:34 +0000

    working on journal app

--- 04_journal_and_file_io/program.py
+++ 04_journal_and_file_io/program.py
@@ -6,7 +6,36 @@ def print_header():
 
 
 def run_event_loop():
-    pass
+    print("What do you want to do with your journal? ")
+
+    cmd = None
+
+    journal_data = []
+
+    while cmd != "x":
+        cmd = input("[L]ist entries, [A]dd an entry, E[x]it: ")
+        cmd = cmd.lower().strip()
+
+        if cmd == "l":
+            list_entries(journal_data)
+        elif cmd == "a":
+            add_entries(journal_data)
+        elif cmd != "x":
+            print(f"Sorry, we dont understand '{cmd}'.")
+
+    print("Done. Goodbye.")
+
+
+def list_entries(data):
+    print("Your journal entries: ")
+    entries = reversed(data)
+    for idx, entry in enumerate(entries, 1):
+        print(f"* [{idx}]: {entry}")
+
+
+def add_entries(data):
+    text = input("Type your entry, <enter> to exit: ")
+    data.append(text)
 
 
 def main():
