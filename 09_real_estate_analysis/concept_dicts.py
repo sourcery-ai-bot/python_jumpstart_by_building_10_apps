538f8b4ee4d6f8e1c7f9ef6b8b5c9ecfaffb009f
vivekvashist <vivekvashist@gmail.com>
2019-12-02 11:42:11 +0000

    worked on app 09

---
+++ 09_real_estate_analysis/concept_dicts.py
@@ -0,0 +1,55 @@
+lookup = {}
+lookup = dict()
+lookup = {"age": 42, "loc": "Italy"}
+lookup = dict(age=42, loc="Italy")
+
+print(lookup)
+print(lookup["loc"])
+
+# lookup['cat'] # keyerror
+
+
+class Wizard(object):
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+
+
+gandolf = Wizard("Glandof", 42)
+print(gandolf.__dict__)
+
+
+# Suppose these came from a data source, e.g. database, web service, etc
+# And we want to randomly access them
+
+import collections
+
+User = collections.namedtuple("User", "id, name, email")
+
+users = [
+    User(1, "user1", "user1@talkpython.fm"),
+    User(2, "user2", "user2@talkpython.fm"),
+    User(3, "user3", "user3@talkpython.fm"),
+    User(4, "user4", "user4@talkpython.fm"),
+]
+
+
+for u in users:
+    print(u)
+
+print()
+print()
+
+for u in users:
+    print(u.id)
+
+print()
+print()
+
+lookup = dict()
+
+for u in users:
+    lookup[u.id] = u
+
+print(lookup)
+
--- 09_real_estate_analysis/program.py
+++ 09_real_estate_analysis/program.py
@@ -1,4 +1,7 @@
 import pathlib
+import csv
+import statistics
+import data_types
 
 
 def print_header():
@@ -15,20 +18,88 @@ def get_data_file():
 
 def load_file(filename):
     with open(filename, "r", encoding="utf-8") as fin:
-        header = fin.readline().strip()
-        print(f"found header: {header}")
 
-        lines = []
+        reader = csv.DictReader(fin)
 
-        for line in fin:
-            line_data = line.strip().split(",")
-            lines.append(line_data)
+        purchases = []
 
-        print(lines[:6])
+        for row in reader:
+            p = data_types.Purchase.create_from_dict(row)
+            purchases.append(p)
+
+        return purchases
+
+        # print(purchases[0].__dict__)
+
+        # for row in reader:
+        #     print(f"Bed count: {row['beds']}, Type: {type(row['beds'])}")
+
+        # general csv.reader()
+        # header = fin.readline().strip()
+        # reader = csv.reader(fin, delimiter=",")
+        # for row in reader:
+        #     print(type(row), row)
+
+
+# def load_file_basic(filename):
+#     with open(filename, "r", encoding="utf-8") as fin:
+#         header = fin.readline().strip()
+#         print(f"found header: {header}")
+
+#         lines = []
+
+#         for line in fin:
+#             line_data = line.strip().split(",")
+#             lines.append(line_data)
+
+#         print(lines[:6])
+
+#
+# def get_price(p):
+# return p.price
+#
 
 
 def query_data(data):
-    pass
+    # most expensive house ?
+
+    data.sort(key=lambda p: p.price)
+    high_purchase = data[-1]
+
+    print(
+        f"The most expensive house is ${high_purchase.price} with {high_purchase.beds} and {high_purchase.baths}."
+    )
+    # least expensive house ?
+
+    low_purchase = data[0]
+    print(
+        f"The most least house is ${low_purchase.price} with {low_purchase.beds} and {low_purchase.baths}."
+    )
+
+    # average price house ?
+
+    prices = [p.price for p in data]  # project of items  # set to process
+
+    ave_price = statistics.mean(prices)
+    print(f"The average home price is ${int(ave_price):,}.")
+
+    prices = [p.price for p in data if p.beds == 2]
+
+    # average price of 2 bedroom houses ?
+
+    two_bed_homes = [p for p in data if p.beds == 2]
+    # projection
+    # source
+    # filter
+
+    avg_price = statistics.mean([p.price for p in two_bed_homes])
+    avg_baths = statistics.mean([p.baths for p in two_bed_homes])
+    avg_sqft = statistics.mean([p.sq__ft for p in two_bed_homes])
+
+    ave_price = statistics.mean(prices)
+    print(
+        f"The average price of a 2-bedroom home is ${int(ave_price):,} with baths={round(avg_baths, 1):,}, sq ft={round(avg_sqft, 1):,}."
+    )
 
 
 def main():
